34772e2d351de32d75e78f5326759dbf83f7040f
Chandru Bharathy <chandrubharathy@Chandrus-MacBook-Air.local>
2021-10-13 01:59:20 +0530

    dev changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World , how are you  !!!!
+         Hello World , how are you  Welcome to dev branch!!!!
         </p>
         <a
           className="App-link"
